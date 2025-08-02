8f16dea6e8c883140e92abf8bdc34cd3a9d65961
Huy Pham <huypham060398@gmail.com>
2025-08-03 00:39:31 +0300

    minor updates

--- webui/next.config.mjs
+++ webui/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  allowedDevOrigins: ['localhost:3001', "127.0.0.1:3001"],
   eslint: {
     ignoreDuringBuilds: true,
   },
