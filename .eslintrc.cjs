2aa310fcd2b6e38be00b733482446571019f9596
Huy Pham <huypham060398@gmail.com>
2025-09-04 03:08:57 +0300

    remove malicious airbnb-base package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/typescript',
   ],
