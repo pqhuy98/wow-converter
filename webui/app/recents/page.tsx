1c1ff9dd3e1bfebdcfcb487b5c0b4d2db05fb7e4
Huy Pham <huypham060398@gmail.com>
2025-09-11 02:28:06 +0300

    fix recent export bug

--- webui/app/recents/page.tsx
+++ webui/app/recents/page.tsx
@@ -147,7 +147,7 @@ export default function RecentsPage() {
       const res = await fetch('/api/download', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ files }),
+        body: JSON.stringify({ files: files.map(({ path }) => path) }),
       });
 
       if (!res.ok) {
